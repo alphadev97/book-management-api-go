72dde07af59411a28bef78ea0544b1f182bdec0e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-01 21:18:18 +0500

    Parsing issue resolved

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -7,25 +7,11 @@ import (
 )
 
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := io.ReadAll(r.Body); err != nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
-			return
-		}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	if err := json.Unmarshal(body, x); err != nil {
+		return
 	}
 }
-
-// package utils
-
-// import (
-// 	"encoding/json"
-// 	"io"
-// 	"net/http"
-// )
-
-// func ParseBody(r *http.Request, x interface{}) {
-// 	if body, err := io.ReadAll(r.Body); err == nil {  // Corrected condition
-// 		if err := json.Unmarshal(body, x); err != nil {  // Removed unnecessary conversion
-// 			return
-// 		}
-// 	}
-// }
